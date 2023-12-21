9947807de8391f2d4264296e7ca47321efba0eac
Hemant28codes <hemantgoyal748@gmail.com>
2023-12-21 14:46:44 +0530

    Built Sign In and Sign Up

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 
 const Login = () => {
+
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  }
+
   return (
     <div>
       <Header />
@@ -11,11 +17,13 @@ const Login = () => {
           alt="background"
         />
       </div>
-      <form className="p-12 bg-black absolute w-3/12 my-36 mx-auto right-0 left-0 text-white">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
-        <input type="text" placeholder="Email" className="p-2 my-2 w-full" />
-        <input type="password" placeholder="Password" className="p-2 my-2 w-full" /> 
-        <button className="p-4 my-4 bg-red-700 w-full">Sign In</button>
+      <form className="p-12 bg-black absolute w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+        {!isSignInForm && (<input type="text" placeholder="Name" className="p-4 my-4 w-full bg-gray-700" />)}
+        <input type="text" placeholder="Email" className="p-4 my-4 w-full bg-gray-700" />
+        <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700" /> 
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm ? "Not an existing user? Sign Up Now" : "Already registered? Sign In Now"}</p>
       </form>
     </div>
   );
