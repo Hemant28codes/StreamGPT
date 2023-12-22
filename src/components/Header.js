dff12a490e8069c0a544febc7202b4bc9f30e761
Hemant28codes <hemantgoyal748@gmail.com>
2023-12-22 18:26:25 +0530

    GPT Search page with multilingual support

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,14 @@ import { signOut, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from '../utils/userSlice';
+import { toggleGptSearchView } from '../utils/gptSlice';
+import { SUPPORTED_LANGUAGES } from '../utils/constants';
+import { changeLanguage } from '../utils/configSlice';
 
 const Header = () => {
+
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+  const user = useSelector((store) => store.user);
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -24,7 +30,7 @@ const Header = () => {
   },[])
 
   const navigate = useNavigate();
-  const user = useSelector((store) => store.user);
+  
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
@@ -33,11 +39,23 @@ const Header = () => {
     });
   }
 
+  const handleGptSearchClick = () => {
+    dispatch(toggleGptSearchView());
+  }
+  
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  }
+
   return (
     <div className='flex justify-between absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10'>
       <img className='w-44' src={require('../assets/logo.png')} alt="logo" />
       {
         user && <div className='flex p-2'>
+        {showGptSearch && <select className='bg-gray-900 rounded-lg mx-2 text-white' onChange={handleLanguageChange}>
+          {SUPPORTED_LANGUAGES.map(lang => <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
+        </select>}
+        <button className='bg-purple-800 p-2 text-white rounded-lg mx-4' onClick = {handleGptSearchClick}>{showGptSearch ? "HomePage" : "GPT Search"}</button>
           <img className='w-12 h-9' alt="userIcon" src={require('../assets/user.png')} />
           <button className="font-bold text-white" onClick={handleSignOut}>Sign Out</button>
         </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,7 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfi
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { BG_URL } from "../utils/constants";
 
 
 const Login = () => {
@@ -71,7 +72,7 @@ const Login = () => {
       <Header />
       <div className="absolute">
         <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/ca6a7616-0acb-4bc5-be25-c4deef0419a7/c5af601a-6657-4531-8f82-22e629a3795e/IN-en-20231211-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          src={BG_URL}
           alt="background"
         />
       </div>
